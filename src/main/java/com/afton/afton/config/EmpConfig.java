1adf48e510870fc8149a55843ec48dfa201a12e5
Saitej-Ravipati <126831778+Saitej-Ravipati@users.noreply.github.com>
2023-03-08 10:41:11 -0600

    space removed

--- src/main/java/com/afton/afton/config/EmpConfig.java
+++ src/main/java/com/afton/afton/config/EmpConfig.java
@@ -4,10 +4,8 @@ import com.afton.afton.service.EmployeeService;
 import com.afton.afton.service.EmployeeServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class EmpConfig {
-
     @Bean
     public EmployeeService employeeService(){
 return new EmployeeServiceImpl();
